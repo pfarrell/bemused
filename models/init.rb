5c0dd7318f5fbcacb8d891e57db56aa644bc4a0d
Pat Farrell <pfarrell@ulive.com>
2014-06-10 21:47:17 -0700

    db env vars

--- models/init.rb
+++ models/init.rb
@@ -1,6 +1,6 @@
 require 'sequel'
 
-DB = Sequel.connect('mysql://root:root@localhost/pshare')
+DB = Sequel.connect("mysql://#{ENV["BEMUSED_DB_USER"]}:#{ENV["BEMUSED_DB_PASS"]}@#{ENV["BEMUSED_DB_HOST"]}/#{ENV["BEMUSED_DB_DB"]}")
 
 require_relative 'track'
 require_relative 'album'
