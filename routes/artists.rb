54876d0289555c308d1ff3c8768281f8bf3354f8
Pat Farrell <pfarrell@ulive.com>
2014-06-20 23:14:53 -0700

    edit images

--- routes/artists.rb
+++ routes/artists.rb
@@ -1,15 +1,29 @@
+require 'open-uri'
+
 class Bemused < Sinatra::Application
   get "/artist/:id" do
     haml :artist, :locals => {:artist=> Artist[params[:id]]} 
   end
 
   get "/admin/artist/:id" do
-    haml :"admin/model", locals: {model: Artist[params[:id]]}
+    haml :"admin/artist", locals: {model: Artist[params[:id]]}
   end
 
   post "/admin/artist/:id" do
     artist = Artist[params[:id]].merge_params(params)
     artist.save
     haml :artist, locals: {artist: artist}
+  end
+
+  post "/admin/artist/:id/image" do
+    artist = Artist[params[:id]]
+    open("#{params[:image_url]}") {|f|
+      File.open("public/images/artists/#{params[:image_name]}", "wb") do |file|
+        file.puts f.read
+      end
+    }
+    artist.image_path="#{params[:image_name]}"
+    artist.save
+    redirect("/admin/artist/#{artist.id}")
   end
 end
