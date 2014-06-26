5ea466479b0968a6c3abfb27537b0712eb6195ac
Patrick Farrell <pfarrell@prosper.com>
2014-06-26 15:47:26 -0700

    redirect to root doc if search is 0 or 1 characters

--- routes/search.rb
+++ routes/search.rb
@@ -1,7 +1,7 @@
 class Bemused < Sinatra::Application
   get "/search" do
     query = params[:q]
-    redirect(url_for("/")) if query.length < 2
+    redirect(url_for("/")) if query.nil? || query.length < 2
     haml :search, locals: {
       :albums => Album.where(Sequel.ilike(:title, "%#{query}%")),
       :artists => Artist.where(Sequel.ilike(:name, "%#{query}%"))
