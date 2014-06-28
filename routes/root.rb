2da90808c0ddf53391870acadb1b4d1e904fcf31
Pat Farrell <pfarrell@ulive.com>
2014-06-27 22:38:03 -0700

    increase index count... infinite would be better..

--- routes/root.rb
+++ routes/root.rb
@@ -1,6 +1,6 @@
 class Bemused < Sinatra::Application
   get "/" do
     puts "x_mobile_device: #{request.env['X_MOBILE_DEVICE']}"
-    haml :index, locals: {artists: Artist.exclude(image_path: nil).order(Sequel.lit("RAND()")).limit(12)}
+    haml :index, locals: {artists: Artist.exclude(image_path: nil).order(Sequel.lit("RAND()")).limit(25)}
   end
 end
