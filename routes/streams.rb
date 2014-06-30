c20544a4b3182e6e29073c96317744f6a0475b4d
Patrick Farrell <mr.patfarrell@gmail.com>
2014-06-30 12:19:27 -0700

    pull artist from album, not track

--- routes/streams.rb
+++ routes/streams.rb
@@ -1,7 +1,7 @@
 class Bemused < Sinatra::Application
   get "/stream/:id" do
     track = Track[params[:id]]
-    track.log_event(track, track.album, track.artist, request.ip, 'stream')
+    track.log_event(track, track.album, track.album.artist, request.ip, 'stream')
     send_file "#{track.media_file.absolute_path}"
   end
 end
