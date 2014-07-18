f0bf8df05a94af00211c23489bdf05f8f70029d2
Pat Farrell <pfarrell@ulive.com>
2014-07-17 22:20:09 -0700

    changed command to >

--- routes/search.rb
+++ routes/search.rb
@@ -1,7 +1,7 @@
 class Bemused < Sinatra::Application
   get "/search" do
     query = params[:q]
-    redirect(url_for("/#{query[2..-1]}")) if query =~ /^>>/
+    redirect(url_for("/#{query[1..-1]}")) if query =~ /^>/
     redirect(url_for("/")) if query.nil? || query.length < 2
     haml :search, locals: {
       :albums => Album.where(Sequel.ilike(:title, "%#{query}%")),
