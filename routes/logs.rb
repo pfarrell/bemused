6c44a692f491847745fe99d401bdff9f7bc296e1
Patrick Farrell <mr.patfarrell@gmail.com>
2014-07-24 13:13:15 -0700

    altered logs to return in descending order

--- routes/logs.rb
+++ routes/logs.rb
@@ -1,6 +1,7 @@
 class Bemused < Sinatra::Application
   get "/logs" do
     "hello from track"
-    haml :logs, locals: {logs: Log.all}
+ #   haml :logs, locals: {logs: Log.all}
+     haml :logs, locals: {logs: Log.order(Sequel.desc(:id)).all}
   end
 end
