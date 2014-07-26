cca8252d8d9606c78d559b4afa84f7b1c25c2211
Pat Farrell <pfarrell@ulive.com>
2014-07-26 08:32:28 -0700

    set content-type header on streams

--- routes/streams.rb
+++ routes/streams.rb
@@ -2,6 +2,7 @@ class Bemused < Sinatra::Application
   get "/stream/:id" do
     track = Track[params[:id]]
     track.log_event(track, track.album, track.album.artist, request.ip, 'stream', request.cookies["bmc"])
+    response.headers['Content-Type'] = 'audio/mpeg'
     send_file "#{track.media_file.absolute_path}"
   end
 end
