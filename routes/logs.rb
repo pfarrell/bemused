d33501e43861b41b9ce5c5b1c54cf5d7f5712f73
Patrick Farrell <mr.patfarrell@gmail.com>
2014-08-22 11:22:42 -0700

    pagination for logs

--- routes/logs.rb
+++ routes/logs.rb
@@ -1,7 +1,10 @@
 class Bemused < Sinatra::Application
-  get "/logs" do
-    "hello from track"
- #   haml :logs, locals: {logs: Log.all}
-     haml :logs, locals: {logs: Log.order(Sequel.desc(:id)).all}
+  get "/logs" do 
+    redirect url_for("/logs/1")
+  end
+
+  get "/logs/:page" do
+     page = params[:page].to_i
+     haml :logs, locals: {logs: Log.order(Sequel.desc(:id)).paginate(page, 25), nxt: page + 1, prev: page - 1}
   end
 end
