6c7e5f7055496625db6d32c2ca9a35f37ff39db1
Patrick Farrell <mr.patfarrell@gmail.com>
2014-08-22 11:25:10 -0700

    pagination

--- models/init.rb
+++ models/init.rb
@@ -7,6 +7,7 @@ DB = Sequel.connect(
   "mysql2://#{ENV["BEMUSED_DB_USER"]}:#{ENV["BEMUSED_DB_PASS"]}@#{ENV["BEMUSED_DB_HOST"]}/#{ENV["BEMUSED_DB_NAME"]}",
   logger: $console)
 DB.sql_log_level = :debug
+DB.extension(:pagination)
 
 Sequel::Model.plugin :timestamps
 Sequel::Model.plugin :json_serializer
