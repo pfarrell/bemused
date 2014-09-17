a9ac65715f21735089a3217b54bd0504dcefe493
Pat Farrell <mr.patfarrell@gmail.com>
2014-09-17 00:04:21 -0700

    tracks and albums can have different artists... duhh

--- bin/queue_handler.rb
+++ bin/queue_handler.rb
@@ -20,14 +20,15 @@ while(true)
 
   #Read tags
   tags = mp3.tags
-  artist_name = coalesce hsh["artist_name"], tags.artist
+  track_artist_name = coalesce tags.artist, hsh["artist_name"]
+  album_artist_name = coalesce hsh["artist_name"], tags.artist
   album_name = coalesce hsh["album_name"], tags.album
   src= hsh["file_name"]
   
   # mv to error dir if no tags, add to error queue
 
   # mv to nas if tagged
-  nas_location = "#{ENV["BEMUSED_UPLOAD_BASE"]}/#{artist_name}/#{album_name}/#{File.basename(mp3.tags.source)}"
+  nas_location = "#{ENV["BEMUSED_UPLOAD_BASE"]}/#{album_artist_name}/#{album_name}/#{File.basename(mp3.tags.source)}"
 
   FileUtils.mkdir_p(File.dirname(nas_location))
   begin
@@ -36,9 +37,10 @@ while(true)
   end
 
   
-  artist = Artist.find_or_create(name: artist_name)
-  album = Album.find_or_create(artist: artist, title: album_name)
-  track = Track.find_or_create(artist: artist, album: album, title: tags.title)
+  track_artist = Artist.find_or_create(name: track_artist_name)
+  album_artist = Artist.find_or_create(name: album_artist_name)
+  album = Album.find_or_create(artist: album_artist, title: album_name)
+  track = Track.find_or_create(artist: track_artist, album: album, title: tags.title)
   track.track_number = tags.track_nr
   track.save
   file = MediaFile.find_or_create(absolute_path: nas_location)
