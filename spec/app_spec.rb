bd3c98243f7316660a5fa4ac6c9a639ff15ddff2
Patrick Farrell <mr.patfarrell@gmail.com>
2014-10-10 15:40:52 -0700

    new route tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -25,16 +25,86 @@ describe 'App' do
     expect(last_response.body).to match(/Bemused/)
   end
 
-  it "has an artists route" do
-    get "/artists"
-    #expect(last_response).to be_ok
+  it "has a logs route" do
+    get "/logs"
+    expect(last_response).to be_redirect
+    follow_redirect!
+    puts last_request.url
     expect(last_response.body).to match(/Bemused/)
   end
 
-  it "has a logs route" do
-    get "/log"
-    #expect(last_response).to be_ok
+  it "has a top route" do
+    get "/top"
+    expect(last_response).to be_ok
     expect(last_response.body).to match(/Bemused/)
   end
+
+  it "has an active route" do
+    get "/active"
+    expect(last_response).to be_ok
+    expect(last_response.body).to match(/Bemused/)
+  end
+
+  it "has an newborns route" do
+    get "/newborns"
+    expect(last_response).to be_ok
+    expect(last_response.body).to match(/Bemused/)
+  end
+
+  it "has a rand route" do
+    get "/rand"
+    expect(last_response).to be_ok
+    expect(last_response.body).to match(/Bemused/)
+  end           
+
+  it "has a surprise route" do
+    get "/surprise"
+    expect(last_response).to be_ok
+    expect(last_response.body).to match(/Bemused/)
+  end           
+
+  it "has a livesearch route" do
+    get "/livesearch?q=w"
+    expect(last_response).to be_ok
+    expect(last_response.body).to match(/suggestions/)
+  end           
+
+  it "has a searchtracks route" do
+    get "/searchtracks?q=w"
+    expect(last_response).to be_ok
+    expect(last_response.body).to match(/suggestions/)
+  end           
+
+  it "has a track route" do
+    get "/search?q=wax"
+    expect(last_response).to be_ok
+    expect(last_response.body).to match(/Bemused/)
+  end           
+
+  it "has a random track route" do
+    get "/track/random"
+    expect(last_response).to be_ok
+    expect(last_response.body).to match(/^\[\{.*\}\]$/)
+  end           
+
+  it "has a tracks route" do
+    get "/tracks"
+    expect(last_response).to be_ok
+    expect(last_response.body).to match(/Bemused/)
+  end           
+
+  it "has a tracks route with a search" do
+    get "/tracks?q=the"
+    expect(last_response).to be_ok
+    expect(last_response.body).to match(/Bemused/)
+  end           
+
+  it "has a search route" do
+    get "/search?q=wax"
+    expect(last_response).to be_ok
+    expect(last_response.body).to match(/Bemused/)
+  end           
+
+ 
 
 end
