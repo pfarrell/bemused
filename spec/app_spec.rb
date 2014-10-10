85b97c9f0f5e7c19bbf57593e60fec956fe31e63
Patrick Farrell <mr.patfarrell@gmail.com>
2014-10-10 14:54:01 -0700

    moar route test

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -7,9 +7,34 @@ describe 'App' do
     expect(last_response.body).to match(/Bemused/)
   end
 
-  it "should allow access to the home page with query string" do
-    get "/?name=Pat"
+  it "has a radio route" do
+    get "/radio"
     expect(last_response).to be_ok
     expect(last_response.body).to match(/Bemused/)
   end
+
+  it "has an upload route" do
+    get "/upload"
+    expect(last_response).to be_ok
+    expect(last_response.body).to match(/Bemused/)
+  end
+
+  it "has a playlists route" do
+    get "/playlists"
+    expect(last_response).to be_ok
+    expect(last_response.body).to match(/Bemused/)
+  end
+
+  it "has an artists route" do
+    get "/artists"
+    #expect(last_response).to be_ok
+    expect(last_response.body).to match(/Bemused/)
+  end
+
+  it "has a logs route" do
+    get "/log"
+    #expect(last_response).to be_ok
+    expect(last_response.body).to match(/Bemused/)
+  end
+
 end
