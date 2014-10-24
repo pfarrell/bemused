f8af02915c8decd327640364af251070c5121803
Pat Farrell <mr.patfarrell@gmail.com>
2014-10-23 22:56:45 -0700

    flag missing

--- bin/flag_missing.rb
+++ bin/flag_missing.rb
@@ -3,5 +3,6 @@ $: << File.expand_path('../../app', __FILE__)
 require 'models'
 
 MediaFile.where('discriminator = :disc', disc: 'mp3').each do |f|
-  puts f.absolute_path unless File.exists?(f.absolute_path)
+  puts "#{f.track.title} #{f.absolute_path}" unless f.track.nil? || File.exists?(f.absolute_path)
+ 
 end
