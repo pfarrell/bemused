b1e066ffafc7c3609bd7bc6d064b6f4cd02c7071
Pat Farrell <mr.patfarrell@gmail.com>
2014-10-26 11:02:31 -0700

    prepping for mass extinction

--- bin/flag_missing.rb
+++ bin/flag_missing.rb
@@ -2,7 +2,10 @@ $: << File.expand_path('../../app', __FILE__)
 
 require 'models'
 
+cnt = 0
 MediaFile.where('discriminator = :disc', disc: 'mp3').each do |f|
   puts "#{f.track.title} #{f.absolute_path}" unless f.track.nil? || File.exists?(f.absolute_path)
- 
+  cnt += 1
 end
+
+puts "#{cnt} missing"
