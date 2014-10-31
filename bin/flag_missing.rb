4452303abf32a2553a00f489a3bd2505427b845b
Patrick Farrell <mr.patfarrell@gmail.com>
2014-10-30 22:44:12 -0700

    share2  update

--- bin/flag_missing.rb
+++ bin/flag_missing.rb
@@ -3,13 +3,14 @@ require 'models'
 
 cnt = 0
 cnt_unicode=0
-cnt_var2=0
+cnt_share2=0
 MediaFile.where('discriminator = :disc', disc: 'mp3').each do |f|
   puts "#{f.track.title} #{f.absolute_path}" unless f.track.nil? || File.exists?(f.absolute_path)
   cnt += 1
   cnt_unicode+=1 if f.absolute_path =~ /\?/
-  cnt_var_2+=1 if f.absolute_path =~ /var2/
+  cnt_share2+=1 if f.absolute_path =~ /share2/
 end
 
 puts "#{cnt} missing"
 puts "#{cnt_unicode} possible munged names"
+puts "#{cnt_share2} possible shared names"
