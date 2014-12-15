800875bf92ab7277a9516d84531d3c7c44364ecc
Patrick Farrell <mr.patfarrell@gmail.com>
2014-12-15 13:26:36 -0800

    switch to simpler db conn string

--- app/models.rb
+++ app/models.rb
@@ -3,9 +3,7 @@ require 'redis'
 require 'logger'
   
 $console = Logger.new STDOUT
-DB = Sequel.connect(
-  "mysql2://#{ENV["BEMUSED_DB_USER"]}:#{ENV["BEMUSED_DB_PASS"]}@#{ENV["BEMUSED_DB_HOST"]}/#{ENV["BEMUSED_DB_NAME"]}",
-  logger: $console)
+DB = Sequel.connect(ENV["BEMUSED_DB"], logger: $console)
 DB.sql_log_level = :debug
 DB.extension(:pagination)
 
