da06c8e8fdee5c7c2c042e56da265c019750ce56
Patrick Farrell <mr.patfarrell@gmail.com>
2015-01-31 15:00:46 -0800

    keyboard controls for player and consolidation of functionality to javascript include

---
+++ public/js/keypress.js
@@ -0,0 +1,103 @@
+var fastforward, rewinding, rwaction, wasplaying = null;
+
+function toggle_visible(obj) {
+  obj.is(':visible') ?  obj.fadeOut() : obj.fadeIn();
+}
+
+$(document).keydown(function(event) {
+  if(!$("#q").is(":focus")) {
+    switch(event.which) {
+      case 32: //space bar
+        $(".jp-state-playing").length == 1 ? myPlaylist.pause() : myPlaylist.play();
+        break;
+      case 37:
+        myPlaylist.previous(); //left arrow
+        break;
+      case 39: //right arrow
+        myPlaylist.next();
+        break;
+      case 80: //p
+        toggle_visible($('#jp_container_1')); 
+        break;
+      case 83:
+        myPlaylist.shuffle();
+        break;
+    }
+    console.log(event.which);
+  }
+});
+
+//Handles the key down event (so the user can hold a key down to continue)
+$(document).keydown(function (e) {
+    wasplaying = $(".jp-state-playing").length == 1;
+    //Rewind
+    if (e.keyCode == 66 && (!rewinding)) {
+        rewinding = true;
+        //Pause the player
+        //$("#jquery_jplayer_1").jPlayer("pause");
+        RewindTrack();
+        rwaction = window.setInterval(function () { RewindTrack() }, 200);
+    }
+    else if (e.keyCode == 70 && (!fastforward)) {
+        fastforward = true;
+        //Pause the player
+        //$("#jquery_jplayer_1").jPlayer("pause");
+        FastforwardTrack();
+        ffaction = window.setInterval(function () { FastforwardTrack() }, 200);
+    }
+});
+//Ends the action
+$(document).keyup(function (e) {
+    var returnState = wasplaying ? "play" : "pause";
+    //Rewind
+    if (e.keyCode == 66) {
+      rewinding = false;
+      window.clearInterval(rwaction);
+      $("#jquery_jplayer_1").jPlayer(returnState);
+    }
+    else if (e.keyCode == 70) {
+      fastforward = false;
+      window.clearInterval(ffaction);
+      $("#jquery_jplayer_1").jPlayer(returnState);
+    }
+});
+
+//Related function
+function GetPlayerProgress() {
+    return ($('.jp-play-bar').width() / $('.jp-seek-bar').width() * 100);
+}
+
+//Handles rewinding
+function RewindTrack() {
+    //Get current progress and decrement
+    var currentProgress = GetPlayerProgress();
+    //Rewinds 2% of track length
+    var futureProgress = currentProgress - 1.5;
+    //If it goes past the starting point - stop rewinding and pause
+    if (futureProgress <= 0) {
+        rewinding = false;
+        window.clearInterval(rwaction);
+        //$("#jquery_jplayer_1").jPlayer("pause", 0);
+    }
+    //Continue rewinding
+    else {
+        $("#jquery_jplayer_1").jPlayer("playHead", parseInt(futureProgress, 10));
+    }
+}
+
+//Fast forwards the track
+function FastforwardTrack() {
+    //Get current progress and increment
+    var currentProgress = GetPlayerProgress();
+    //Fast forwards 2%
+    var futureProgress = currentProgress + 1.5;
+    //If the percentage exceeds the max - stop fast forwarding at the end.
+    if (futureProgress >= 100) {
+        fastforward = false;
+        window.clearInterval(ffaction);
+        $("#jquery_jplayer_1").jPlayer("playHead", parseInt($('.jp-duration').text().replace(':', '')));
+    }
+    else {
+        $("#jquery_jplayer_1").jPlayer("playHead", parseInt(futureProgress, 10));
+    }
+}
