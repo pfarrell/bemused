7946f977a89e27c324c2109648b6319268b8fdd7
Patrick Farrell <mr.patfarrell@gmail.com>
2015-03-04 13:44:05 -0800

    add missing indexes to all tables

---
+++ db/migrations/014_add_default_indexes.rb
@@ -0,0 +1,29 @@
+Sequel.migration do
+  change do
+    alter_table(:albums) do
+      add_index :artist_id
+      add_index :title
+    end
+
+    alter_table(:artists) do
+      add_index :name
+    end
+    
+    alter_table(:media_files) do
+      add_index :track_id
+      add_index :absolute_path
+    end
+
+    alter_table(:playlists_tracks) do
+      add_index :playlist_id
+      add_index :track_id
+    end
+
+    alter_table(:tracks) do
+      add_index :album_id
+      add_index :artist_id
+      add_index :media_file_id
+      add_index :title
+    end
+  end
+end
