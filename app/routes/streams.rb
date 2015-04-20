a0e10773f947f3737e7b7ac766d191f02a786a9c
Patrick Farrell <mr.patfarrell@gmail.com>
2015-04-20 08:04:49 -0700

    Revert "log code to attempt to figure out what happens when server goes weird.."
    This reverts commit d9976f7b87af945b772c7d1b65833481460400b6.

--- app/routes/streams.rb
+++ app/routes/streams.rb
@@ -1,8 +1,7 @@
 class Bemused < Sinatra::Application
   get "/stream/:id" do
-    puts "streaming #{params[:id]}"
     track = Track[params[:id]]
-    puts "retrieved/sending #{track.title]}"
+#    track.log_event(track, track.album, track.album.artist, request.ip, 'stream', request.cookies["bmc"])
     response.headers['Content-Type'] = 'audio/mpeg'
     send_file "#{track.media_file.absolute_path}"
   end
