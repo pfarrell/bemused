d8c83084890a0a9563064cd611c4fc98ee8ac770
Patrick Farrell <mr.patfarrell@gmail.com>
2015-08-09 15:52:33 -0700

    paging changes

--- app/routes/logs.rb
+++ app/routes/logs.rb
@@ -1,10 +1,16 @@
 class Bemused < Sinatra::Application
+  def page_seq(curr_page, page_count)
+    start = curr_page > 5 ? curr_page - 5 : 1
+    stop = page_count - curr_page > 5 ? curr_page + 5 : page_count
+    return (start..stop)
+  end
+
   get "/logs" do 
     redirect url_for("/logs/1")
   end
 
   get "/logs/:page" do
      page = params[:page].to_i
-     haml :logs, locals: {logs: Log.order(Sequel.desc(:id)).paginate(page, 25), nxt: page + 1, prev: page - 1}
+     haml :logs, locals: {model: {data: Log.order(Sequel.desc(:id)).paginate(page, 25)}}
   end
 end
