ef62a92efc7e096a2b7a1d8edda5854998be5902
Patrick Farrell <mr.patfarrell@gmail.com>
2015-08-26 10:44:34 -0700

    updated specs for empty database

--- spec/bemused/album_spec.rb
+++ spec/bemused/album_spec.rb
@@ -1,7 +1,26 @@
 require 'spec_helper'
 
 describe Album do
-  let(:album) {Album.first}
+  let(:artist) {
+    Artist.find_or_create(name: "test_generated_artist")
+  }
+
+  let(:track) {
+    track=Track.find_or_create(title: "test_generated_track")
+    track.save
+    track
+  }
+
+  let(:album) {
+    album=Album.find_or_create(title: "test_generated_album")
+    album.artist=artist
+    album.tracks << track
+    album.image_path='test_path/example.jpg'
+    track.album = album
+    track.save
+    album.save
+    album
+  }
   
   it "to_s returns album title" do
     expect(album.to_s).to eq(album.title)
--- spec/bemused/artist_spec.rb
+++ spec/bemused/artist_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe Artist do
-  let(:artist) {Artist.first}
+  let(:artist) {
+    Artist.find_or_create(name: "test_generated_artist")
+  }
   
   it "to_s returns artist name" do
     expect(artist.to_s).to eq(artist.name)
