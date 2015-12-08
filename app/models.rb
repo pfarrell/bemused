a4860ac989af50575b7984074b8b87d4b8ff6542
Patrick Farrell <mr.patfarrell@gmail.com>
2015-12-07 23:10:27 -0800

    Five minute timeout on connection checkout

--- app/models.rb
+++ app/models.rb
@@ -14,6 +14,8 @@ DB.extension(:pagination)
 DB.extension(:pg_array, :pg_json)
 DB.extension(:connection_validator)
 
+DB.pool.connection_validation_timeout = 300
+
 Sequel::Model.plugin :timestamps
 Sequel::Model.plugin :json_serializer
 
