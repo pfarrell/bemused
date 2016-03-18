851e73a56b1613c84a22119f1bf68b73b72e03f7
Patrick Farrell <patrick.farrell@grandrounds.com>
2016-03-18 09:05:32 -0700

    redirect to model edit

--- app/routes/tracks.rb
+++ app/routes/tracks.rb
@@ -32,7 +32,7 @@ class Bemused < Sinatra::Application
   post "/admin/track/:id" do
     track = Track[params[:id]].merge_params(params)
     track.save
-    "track saved"
+    haml :"admin/model", locals: {model: Track[params[:id]]}
   end
 
   get "/tracks/words" do
