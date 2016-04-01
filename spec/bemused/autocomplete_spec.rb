511853bf35cb6a5783c71f6d95b7de1d13534105
Patrick Farrell <patrick.farrell@grandrounds.com>
2016-03-31 23:00:56 -0700

    expand autocomplete tests to include command lookups

--- spec/bemused/autocomplete_spec.rb
+++ spec/bemused/autocomplete_spec.rb
@@ -22,10 +22,57 @@ describe AutoComplete do
     album
   }
 
+  shared_examples_for 'it autocompletes' do |search, command|
+    describe "the #{command} command" do
+      it "#{search} returns shortcut for #{command} command" do
+        expect(AutoComplete.lookup("#{search}")['suggestions'].select{|x| x == {'value'=> "#{command}"}}.size).to eq(1)
+      end
+    end
+  end
+
   it 'returns albums' do
     album
     expect(AutoComplete.lookup('t')['suggestions'].select{|x| x == {'value'=> 'test_generated_album'}}.size).to eq(1)
   end
+
+  it 'returns artists' do
+    album
+    expect(AutoComplete.lookup('t')['suggestions'].select{|x| x == {'value'=> 'test_generated_artist'}}.size).to eq(1)
+  end
+
+  it 'ignores artists without albums' do
+    expect(Artist[artist.id].name).to eq(artist.name)
+    expect(AutoComplete.lookup('t')['suggestions'].select{|x| x == {'value'=> 'test_generated_artist'}}.size).to eq(0)
+  end
+
+  it_behaves_like 'it autocompletes', 'a', "/active"
+  it_behaves_like 'it autocompletes', '/act', "/active"
+  it_behaves_like 'it autocompletes', 'a', "/albums/recent"
+  it_behaves_like 'it autocompletes', 'rec', "/albums/recent"
+  it_behaves_like 'it autocompletes', 'a', "/albums/words"
+  it_behaves_like 'it autocompletes', 'wor', "/albums/words"
+  it_behaves_like 'it autocompletes', 'l', "/logs"
+  it_behaves_like 'it autocompletes', 'log', "/logs"
+  it_behaves_like 'it autocompletes', 'n', "/newborns"
+  it_behaves_like 'it autocompletes', 'newb', "/newborns"
+  it_behaves_like 'it autocompletes', 'p', "/playlists"
+  it_behaves_like 'it autocompletes', '/pl', "/playlists"
+  it_behaves_like 'it autocompletes', 'r', "/radio"
+  it_behaves_like 'it autocompletes', 'radi', "/radio"
+  it_behaves_like 'it autocompletes', 'r', "/random"
+  it_behaves_like 'it autocompletes', '/random', "/random"
+  it_behaves_like 'it autocompletes', 's', "/surprise"
+  it_behaves_like 'it autocompletes', 'pri', "/surprise"
+  it_behaves_like 'it autocompletes', 't', "/top"
+  it_behaves_like 'it autocompletes', 'op', "/top"
+  it_behaves_like 'it autocompletes', 't', "/tracks"
+  it_behaves_like 'it autocompletes', 'acks', "/tracks"
+  it_behaves_like 'it autocompletes', 't', "/tracks/words"
+  it_behaves_like 'it autocompletes', 'WORDS', "/tracks/words"
+  it_behaves_like 'it autocompletes', 't', "/track_paths"
+  it_behaves_like 'it autocompletes', '_', "/track_paths"
+  it_behaves_like 'it autocompletes', 'u', "/upload"
+  it_behaves_like 'it autocompletes', 'UP', "/upload"
 
 end
 
