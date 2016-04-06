63eabb6b9e5d5bdfd050ac8332d85d2b88e5e15c
Patrick Farrell <patrick.farrell@grandrounds.com>
2016-04-06 08:59:07 -0700

    try three forms of album names

--- app/routes/info.rb
+++ app/routes/info.rb
@@ -1,13 +1,29 @@
 class Bemused < Sinatra::Application
 
-  get "/album/:id/summary" do
-    album = Album[params[:id]]
-    title = album.wikipedia || album.title
+  def possible_titles(artist, title)
+    ["#{title} (#{artist} album)", "#{title} (album)", title]
+  end
+
+  def summary(searches)
+    searches.each do |search|
+      summary = lookup(search)
+      return summary unless summary.nil?
+    end
+  end
+
+  def lookup(search)
     begin
-      Info.summary(title)
+      Info.summary(search)
     rescue
       nil
     end
+  end
+
+  get "/album/:id/summary" do
+    album = Album[params[:id]]
+    artist = album.artist.wikipedia || album.artist.name
+    title = album.wikipedia || album.title
+    summary(possible_titles(artist, title))
   end
 
   get "/summary/:search" do
