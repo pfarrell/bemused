5b368be76b633d8462687c30134aedf08987bc38
Patrick Farrell <patrick.farrell@grandrounds.com>
2016-04-05 22:59:57 -0700

    better error handling for wikipedia lookups

--- app/routes/info.rb
+++ app/routes/info.rb
@@ -3,7 +3,11 @@ class Bemused < Sinatra::Application
   get "/album/:id/summary" do
     album = Album[params[:id]]
     title = album.wikipedia || album.title
-    Info.summary(title)
+    begin
+      Info.summary(title)
+    rescue
+      nil
+    end
   end
 
   get "/summary/:search" do
