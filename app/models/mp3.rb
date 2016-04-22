dd46dc31e9f80437c72da81aea04b9fa80badbef
Patrick Farrell <patrick.farrell@grandrounds.com>
2016-04-21 18:49:28 -0700

    remove debug logging

--- app/models/mp3.rb
+++ app/models/mp3.rb
@@ -9,13 +9,13 @@ class Mp3
 
   def import
     ID3Tag.read(File.open(@file_path), :v2) do |tag|
-      puts @file_path
-      puts "artis: " +  tag.artist
-      puts "title: " + tag.title
-      puts "album: " + tag.album
-      puts "year:  " + tag.year
-      puts "nr:    " +  tag.track_nr
-      puts "genre: " + tag.genre
+#      puts @file_path
+#      puts "artis: " +  tag.artist
+#      puts "title: " + tag.title
+#      puts "album: " + tag.album
+#      puts "year:  " + tag.year
+#      puts "nr:    " +  tag.track_nr
+#      puts "genre: " + tag.genre
     end
   end
 
