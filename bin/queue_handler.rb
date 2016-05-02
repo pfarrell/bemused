d8331a752a7dffed75c815e704d7951fd8403334
Patrick Farrell <mr.patfarrell@gmail.com>
2016-05-01 22:18:17 -0700

    better handling of null tags

--- bin/queue_handler.rb
+++ bin/queue_handler.rb
@@ -6,8 +6,13 @@ require 'json'
 require '../app'
 
 def coalesce(first, second)
-  return first unless first.nil? || first == ""
-  return second
+  return first.strip unless first.nil? || first == ""
+  return second ? second.strip : "no tag"
+end
+
+def safe_strip(val)
+  return val.strip unless val.nil?
+  return "not set"
 end
 
 redis = Redis.new
@@ -20,9 +25,9 @@ while(true)
 
   #Read tags
   tags = mp3.tags
-  track_artist_name = coalesce tags.artist.strip, hsh["artist_name"]
-  album_artist_name = coalesce hsh["artist_name"], tags.artist.strip
-  album_name = coalesce hsh["album_name"], tags.album.strip
+  track_artist_name = coalesce tags.artist, hsh["artist_name"]
+  album_artist_name = coalesce hsh["artist_name"], tags.artist
+  album_name = coalesce hsh["album_name"], tags.album
   genre = hsh["genre"]
   src= hsh["file_name"]
 
@@ -41,7 +46,7 @@ while(true)
   track_artist = Artist.find_or_create(name: track_artist_name)
   album_artist = Artist.find_or_create(name: album_artist_name)
   album = Album.find_or_create(artist: album_artist, title: album_name)
-  track = Track.find_or_create(artist: track_artist, album: album, title: tags.title.strip, track_number: tags.track_nr)
+  track = Track.find_or_create(artist: track_artist, album: album, title: safe_strip(tags.title), track_number: tags.track_nr)
   track.track_number = tags.track_nr
   track.save
   file = MediaFile.find_or_create(absolute_path: nas_location)
