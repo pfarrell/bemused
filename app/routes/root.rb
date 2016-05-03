87fc6dcc91b8fb6a07502b2b8be7c25c53332d60
Patrick Farrell <patrick.farrell@grandrounds.com>
2016-05-03 09:43:52 -0700

    up the front page

--- app/routes/root.rb
+++ app/routes/root.rb
@@ -2,6 +2,6 @@ require 'helpers/root_helper'
 class Bemused < Sinatra::Application
   include RootHelper
   get "/" do
-    haml :index, locals: {artists: random_artists(25)}
+    haml :index, locals: {artists: random_artists(50)}
   end
 end
