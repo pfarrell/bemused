115c81cb34eb37642590d541220736761ac4685a
Patrick Farrell <mr.patfarrell@gmail.com>
2016-05-20 23:16:21 -0700

    better logs

--- app/models/log.rb
+++ app/models/log.rb
@@ -5,13 +5,13 @@ class Log < Sequel::Model
   many_to_one :artist
   many_to_one :track
 
-  def self.stats
+  def self.stats(size=10)
     stats = Stat.new(self)
     stats.values = Hash.new{|h,k| h[k] = {}}
     stats.values[:all_time][:count] = Log.count
-    stats.values[:all_time][:popular] = Log.group_and_count(:track_id).order_by(:count).reverse.first(10).map{|log| track = Track[log[:track_id]]; [track, track.album, track.artist, log[:count]]}
-    stats.values[:one_month][:popular] = Log.where{created_at > (Date.today - 30).iso8601}.group_and_count(:track_id).order_by(:count).reverse.first(10).map{|log| track = Track[log[:track_id]]; [track, track.album, track.artist, log[:count]]}
-    stats.values[:one_week][:popular] = Log.where{created_at > (Date.today - 7).iso8601}.group_and_count(:track_id).order_by(:count).reverse.first(10).map{|log| track = Track[log[:track_id]]; [track, track.album, track.artist, log[:count]]}
+    stats.values[:all_time][:popular] = Log.group_and_count(:track_id).order_by(:count).reverse.first(size).map{|log| track = Track[log[:track_id]]; [track, track.album, track.artist, log[:count]]}
+    stats.values[:one_month][:popular] = Log.where{created_at > (Date.today - 30).iso8601}.group_and_count(:track_id).order_by(:count).reverse.first(size).map{|log| track = Track[log[:track_id]]; [track, track.album, track.artist, log[:count]]}
+    stats.values[:one_week][:popular] = Log.where{created_at > (Date.today - 7).iso8601}.group_and_count(:track_id).order_by(:count).reverse.first(size).map{|log| track = Track[log[:track_id]]; [track, track.album, track.artist, log[:count]]}
     stats
   end
 end
