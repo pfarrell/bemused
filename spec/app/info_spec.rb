315309acda4a20bd43ba3085c8df5a187f44a9dc
Patrick Farrell <mr.patfarrell@gmail.com>
2016-05-28 23:05:50 -0700

    better tests

--- spec/app/info_spec.rb
+++ spec/app/info_spec.rb
@@ -2,13 +2,17 @@ require 'spec_helper'
 
 describe Info do
   let(:artist) {Artist.new(name:"test_name")}
-  let(:album) {Album.new(title:"test_title")}
-  let(:track) {Track.new(title:"test_track_title")}
+  let(:album) { Album.new(title:"test_title") }
+  let(:track) { Track.new(title:"test_track_title") }
 
   before do
     artist.save
+    album.artist = artist
     album.save
+    track.artist = artist
+    track.album = album
     track.save
+    get "/log/#{track.id}"
   end
 
   context '#meta' do
