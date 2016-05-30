ede419e6ad8d136e2291b8c2236a120c31347673
Patrick Farrell <mr.patfarrell@gmail.com>
2016-05-29 22:42:07 -0700

    better tests

--- spec/bemused/log_spec.rb
+++ spec/bemused/log_spec.rb
@@ -1,9 +1,25 @@
 require 'spec_helper'
+require 'byebug'
 
 describe Log do
 
+  let(:artist) {Artist.new(name:"test_name")}
+  let(:album) { Album.new(title:"test_title") }
+  let(:track) { Track.new(title:"test_track_title") }
+
+  before do
+    artist.save
+    album.artist = artist
+    album.save
+    track.artist = artist
+    track.album = album
+    track.save
+    get "/log/#{track.id}"
+  end
+
   context '#stats' do
     it 'has a stats class method' do
+      byebug
       expect(Log.stats).to_not be_nil
     end
   end
