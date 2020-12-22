0cf3ddb699c46b7ca707385d54243c85394fe890
Patrick Farrell <mr.patfarrell@gmail.com>
2020-12-21 19:48:28 -0800

    first pass at replacing bower with webpack

---
+++ assets.js
@@ -0,0 +1,16 @@
+const CSS = [
+  'jplayer/dist/skin/blue.monday/css/jplayer.blue.monday.min.css',
+  'bootstrap/dist/css/bootstrap.min.css',
+];
+
+const JS = [
+  'jquery/dist/jquery.min.js',
+  'bootstrap/dist/js/bootstrap.min.js',
+  'jplayer/dist/jplayer/jquery.jplayer.min.js',
+  'jplayer/dist/add-on/jplayer.playlist.js',
+  'masonry/dist/masonry.pkgd.js',
+  'imagesloaded/imagesloaded.pkgd.min.js',
+  'devbridge-autocomplete/dist/jquery.autocomplete.min.js',
+];
+
+module.exports = [...JS, ...CSS];
