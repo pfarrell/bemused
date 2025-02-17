e9f0c5f8854242ae9f7d303e467a8df5acd43851
Patrick Farrell <mr.patfarrell@gmail.com>
2025-02-17 14:56:56 -0500

    better select for random front page

--- app/helpers/root_helper.rb
+++ app/helpers/root_helper.rb
@@ -1,11 +1,12 @@
 module RootHelper
   def random_artists(size)
     artists = context? ? Artist.where(tags: @tags) : Artist
-    artists
-      .join(:albums, artist_id: :id)
+    artists.join(:albums, artist_id: :id)
       .exclude(image_path: nil)
       .qualify
-      .order(Sequel.lit('RANDOM()'))
+      .select_all(:artists)
+      .select_append(Sequel.lit('RANDOM() as random_order'))
+      .distinct.order(:random_order)
       .limit(size)
   end
 
