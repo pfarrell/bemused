3db347928134147ca59a86daa07b79aa73ce9b40
Patrick Farrell <mr.patfarrell@gmail.com>
2025-05-01 22:43:50 -0400

    Adds support for unaccent
    Db migration creates a new function f_unaccent and indexes
    to support ilike queries like "deja vu" to match "Déjà vu"

--- app/helpers/root_helper.rb
+++ app/helpers/root_helper.rb
@@ -15,22 +15,22 @@ module RootHelper
   end
 
   def artists_with_albums(query)
-      Artist.where(Sequel.ilike(:name, "%#{query}%"))
+    Artist.where(Sequel.ilike(Sequel.function(:f_unaccent, :name), "%#{query}%"))
         .join(:albums, artist_id: :id)
         .qualify
         .distinct(:id)
   end
 
   def albums_with_tracks(query)
-    Album.where(Sequel.ilike(:title, "%#{query}%"))
+    Album.where(Sequel.ilike(Sequel.function(:f_unaccent, :title), "%#{query}%"))
       .join(:tracks, album_id: :id)
       .qualify
       .distinct(:id)
   end
 
   def tracks_from_search(query)
-    count = Track.where(Sequel.ilike(:title, "%#{query}%")).count
-    tracks = Track.where(Sequel.ilike(:title, "%#{query}%")).limit(20)
+    count = Track.where(Sequel.ilike(Sequel.function(:f_unaccent, :title), "%#{query}%")).count
+    tracks = Track.where(Sequel.ilike(Sequel.function(:f_unaccent, :title), "%#{query}%")).limit(20)
     return {count: count, tracks: tracks}
   end
 end
