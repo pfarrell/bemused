5d4a069f69eed40cb2550c2cc4bcc429a6b40f07
Patrick Farrell <mr.patfarrell@gmail.com>
2025-06-16 13:51:17 -0400

    update fetch to handle change in url between local dev and prod

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Stats from './pages/Stats';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Stats />);
+  
