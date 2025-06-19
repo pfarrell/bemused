ea7f67068048653655434e37499b39c02e03cfd5
Patrick Farrell <mr.patfarrell@gmail.com>
2025-06-19 17:19:36 -0400

    fix for front end resolution in prod

--- app/routes/root.rb
+++ app/routes/root.rb
@@ -11,13 +11,26 @@ class Bemused < Sinatra::Application
       wants.html { haml :index, layout: !request.xhr?, locals: data }
     end
   end
-  # Serve the React frontend
+
+  # Serve the React frontend assets
   get '/frontend/*' do
-    send_file File.join(settings.public_folder, 'frontend', params['splat'].first)
+    file_path = params['splat'].first
+    full_path = File.join(settings.public_folder, 'frontend', file_path)
+
+    if File.exist?(full_path)
+      send_file full_path
+    else
+      halt 404
+    end
   end
 
-  # Serve the main frontend app
+  # Serve the main frontend app for SPA routes
   get '/app' do
+    send_file File.join(settings.public_folder, 'frontend', 'index.html')
+  end
+
+  # Handle SPA routing - catch frontend routes and serve index.html
+  get '/app/*' do
     send_file File.join(settings.public_folder, 'frontend', 'index.html')
   end
 
